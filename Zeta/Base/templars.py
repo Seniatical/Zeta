ea0b227bf61a7751e5a2bb6691faf89e75433da0
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-02-06 18:23:24 +0000

    Add files via upload

---
+++ Zeta/Base/templars.py
@@ -0,0 +1,15 @@
+class TooManyArgs(Warning):
+    pass
+
+''' Errors Caused with the Hashmap '''
+class DuplicateKey(Exception):
+    pass
+
+class KeyAdditionError(Exception):
+    pass
+
+class KeyNotFound(Exception):
+    pass
+
+class KeyEditError(Exception):
+    pass
