941132daddfd0a3a94b03c66c3183d5fc4ceb488
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-02-06 20:03:07 +0000

    Update __init__.py

--- Zeta/__init__.py
+++ Zeta/__init__.py
@@ -4,6 +4,6 @@ Somebody type up copyright bs
 __author__ = 'Seniatical'
 __version__ = '0.0.1'
 
-from .Base.Creator import Hash
-from .Base.Creator import encryptions
-from .Base.Hashmap import Hashmap
+from Zeta.Base.Creator import Hash
+from Zeta.Base.Creator import encryptions
+from Zeta.Base.Hashmap import Hashmap
