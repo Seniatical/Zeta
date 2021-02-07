3947c2dd49ede2760eb3943c675414d0e869f4f5
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-02-07 11:06:41 +0000

    Full fixes to the module

---
+++ Zeta/Base/Models/__init__.py
@@ -0,0 +1,13 @@
+'''
+Somebody write up a copyright notice please...
+'''
+
+from .Encryption_Patterns import *
+
+from .Model1 import Sen
+from .Model2 import Ceaser
+from .Model3 import Morse
+
+__models__ = (
+	'Sen', 'Ceaser', 'Morse'
+	)
---
+++ Zeta/__init__.py
@@ -0,0 +1,10 @@
+'''
+Somebody type up copyright bs
+'''
+__author__ = 'Seniatical'
+__version__ = '0.0.1'
+
+from .Base.Creator import Hash
+from .Base.Creator import encryptions
+from .Base import Models
+from .Base.Hashmap import Hashmap
