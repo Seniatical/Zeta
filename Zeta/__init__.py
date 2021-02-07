bf2757e9a1c20c99428dbecab920c3d8f31b6107
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-02-07 11:05:51 +0000

    Delete Zeta directory

--- Zeta/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-Somebody type up copyright bs
-'''
-__author__ = 'Seniatical'
-__version__ = '0.0.1'
-
-from Zeta.Base.Creator import Hash
-from Zeta.Base.Creator import encryptions
-from Zeta.Base.Hashmap import Hashmap
